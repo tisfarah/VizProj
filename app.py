2cf83176d49734b07f7a5f9638942ae417c2629c
Francis Kabongo <cescosmalto@gmail.com>
2019-06-22 19:44:40 -0400

    I have added 2 sqlite

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-import os
+# importing the database that we'll need to create our visualization
 
+import os
 import pandas as pd
 import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
 from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 
 
 #################################################
 # Database Setup
 #################################################
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data/Group_Project_NYC.sqlite"
+db = SQLAlchemy(app)
+
 
 
 
