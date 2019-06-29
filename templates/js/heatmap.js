be13f3b016cb90a558920e736cb36859772f81db
Grant <lorig@Amer.Corp.Local>
2019-06-29 19:05:57 -0400

    lg 0629pm0705 heat map working

---
+++ templates/js/heatmap.js
@@ -0,0 +1,42 @@
+// NYC lat/long = 40.730610, -73.935242
+// SFO lat/long = 37.7749, -122.4194 
+
+var HeatMap = L.map("h_map", {
+  center: [40.730610, -73.935242],
+  zoom: 13
+});
+
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(HeatMap);
+
+//var url = "https://data.sfgov.org/resource/cuks-n6tp.json?$limit=10000"; //original SFO line of code
+var url = "http://127.0.0.1:5000/heatData"; //NYC
+
+d3.json(url, function(response) {
+  console.log("-- Next line is the json data")
+  console.log(response);
+
+  var heatArray = [];
+  console.log("-- Next line is the heatArray list.   Should be empty")
+  console.log(heatArray)
+
+  for (var i = 0; i < response.length; i++) {
+    var locations = response[i];
+    
+    // console.log(locations)
+
+    if (locations) {
+      heatArray.push([locations.lat, locations.long]);
+    }
+  }
+
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
+  }).addTo(HeatMap);
+
+});
