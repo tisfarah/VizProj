1d42daa3642a8e7d285b1210c7a43c1b10705ef8
AJ <AJ@users.noreply.github.com>
2019-06-30 18:05:34 -0400

    added more logic to tabularData route

--- templates/static/js/frappetable.js
+++ templates/static/js/frappetable.js
@@ -1,11 +1,33 @@
-var datatable = new DataTable('#datatable', {
-	columns: ['Name', 'Position', 'Salary'],
-  data: [
-  	['Faris', 'Software Developer', '$1200'],
-    ['Manas', 'Software Engineer', '$1400'],
-  ]
-})
+/* data route */
+var url = "http://127.0.0.1:5000/tabularData";
+console.log(url);
+function buildFrappe() {
+  d3.json(url).then(function(response) {
+    console.log(response);
+    var options = {
+      columns: response.map(data => data.columns),
+      data: response.map(data => data.results)
+      //name: "Bigfoot Sightings",
+      //x: response.map(data => data.year),
+      //y: response.map(data => data.sightings),
+      //line: {color: "#17BECF"}
+    };
 
+var data = [options];
+var container = document.querySelector('#datatable');
+var datatable = new DataTable(container, data);
+  });
+}
+  
+buildFrappe();
+
+// var datatable = new DataTable('#datatable', {
+//     columns: ['Name', 'Position', 'Salary'],
+//     data: [
+//         ['Faris', 'Software Developer', '$1200'],
+//     ['Manas', 'Software Engineer', '$1400'],
+//     ]
+// })
 
 
 
